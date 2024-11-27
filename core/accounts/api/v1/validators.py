9c8741b3ea411d0240375f60f8c3108e6da61758
hosseinpk <hosseinpakaein@gmail.com>
2024-11-27 14:31:32 +0330

    follower,private and personal code

--- core/accounts/api/v1/validators.py
+++ core/accounts/api/v1/validators.py
@@ -26,3 +26,19 @@ def special_character_validator(password):
             _("password must include specific character"),
             code="password_must_include_specific_character",
         )
+
+def personal_code_validator(value: str):
+    if not len(value) == 10:
+        raise ValidationError(_({"details" : "personal code should be 10digits"}))
+    
+    res = 0
+    for i, num in enumerate(value[:-1]):
+        res = res + (int(num) * (10 - i))
+
+    remain = res % 11
+    if remain < 2:
+        if not remain == int(value[-1]):
+            raise ValidationError(_({"details" : "wrong persoal code"}))
+    else:
+        if not (11 - remain) == int(value[-1]):
+            raise ValidationError(_({"details" : "wrong persoal code"}))
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -5,7 +5,12 @@ from django.contrib.auth.models import (
     BaseUserManager,
 )
 from django.utils.translation import gettext_lazy as _
-from django.core.validators import EmailValidator
+from django.core.validators import EmailValidator, RegexValidator
+from django.core.exceptions import ValidationError
+from accounts.api.v1.validators import personal_code_validator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
@@ -53,14 +58,39 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+phone_number_validator = RegexValidator(
+    regex=r"^(\+98|0)?9\d{9}$",  # Example regex for phone numbers
+    message="Phone number must be entered in the format: '+989*****' or '09*******'. Up to 13 digits allowed.",
+)
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField("User", on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     image = models.ImageField(upload_to="media/profile", blank=True, null=True)
     bio = models.CharField(max_length=50, blank=True, null=True)
+    personal_code = models.CharField(max_length=10,unique=True,blank=True,null=True,validators=[personal_code_validator,])
+    phone_number = models.CharField( max_length=13, unique=True,blank=True,null=True,validators=[phone_number_validator])
+    follower = models.ManyToManyField("self",symmetrical=False,related_name='following',blank=True)
+    private = models.BooleanField(default=False)
+    #posts = models.ForeignKey()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    def add_follower(self, profile):
+        if profile == self:
+            raise ValueError(" cannot follow yourself.")
+        self.follower.add(profile)
 
     def __str__(self):
         return self.user.email
+    
+    # @receiver(post_save, sender=User)
+    # def save_profile(sender, instance, created, **kwargs):
+    #     """
+    #     Signal for post creating a user which activates when a user being created ONLY
+    #     """
+    #     if created:
+    #         Profile.objects.create(user=instance)
