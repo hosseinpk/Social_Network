f3abb2f059d0d3968de540c59bbb68b239b55c24
hossein <hosseinpakaein@gmail.com>
2025-10-07 20:08:02 +0000

    correct setting.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -146,12 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-<<<<<<< HEAD
 STATIC_URL = "static/"
 
-=======
-STATIC_URL = "/static/"
->>>>>>> dev
 STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
